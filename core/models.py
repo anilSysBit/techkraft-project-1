b3bd34f604f1e771c9e7db86704a47a6b704d03f
anilwagle808 <anil.wagle808@gmail.com>
2026-03-26 20:02:23 +0545

    Created a usermanager to solve issue in create superuser

--- core/models.py
+++ core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import MinValueValidator
 from django.conf import settings
 from django.utils import timezone
@@ -17,6 +17,36 @@ class UserRoleChoices(models.TextChoices):
     BUYER = 'buyer', 'Buyer'
     ADMIN = 'admin', 'Admin'
 
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, full_name=full_name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        extra_fields.setdefault("role", "admin")
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(
+            email=email,
+            full_name=full_name,
+            password=password,
+            **extra_fields
+        )
+
 
 class User(AbstractUser):
     """
@@ -29,6 +59,8 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['full_name']
+
+    objects = UserManager()
 
     def __str__(self):
         return f"{self.full_name} ({self.email})"
